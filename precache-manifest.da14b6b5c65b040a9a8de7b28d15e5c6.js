b6f3f1ab175e5f2d2afad196c14096240ebf52b2
portellaa <luis.portela@youclap.tech>
2019-09-30 09:51:09 +0100

    Initial landing page

---
+++ precache-manifest.da14b6b5c65b040a9a8de7b28d15e5c6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beb54e1656ca6c10f9061bfa6e37f5d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1c971642f3b75bfacb25",
+    "url": "/static/css/main.fa2d4031.chunk.css"
+  },
+  {
+    "revision": "4e6a288bfcc13ff1e0e2",
+    "url": "/static/js/2.ab0a1e33.chunk.js"
+  },
+  {
+    "revision": "1c971642f3b75bfacb25",
+    "url": "/static/js/main.f2d9a4b9.chunk.js"
+  },
+  {
+    "revision": "e6c9fb6109b0bef35f64",
+    "url": "/static/js/runtime-main.560bf31a.js"
+  }
+]);
